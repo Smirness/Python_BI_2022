df40397145d5964ccccf0ee540e3a836f0044df2
Viktor_S <102161865+Smirness@users.noreply.github.com>
2022-11-17 09:16:04 +0300

    Delete homework_7 directory

--- homework_7/HW7_README.txt
+++
@@ -1,4 +0,0 @@
-Task 1. /homework_7/ftp_links.py
-Tasks 2-5. /homework_7/finding_in_2430AD.py
-Task 6. /homework_7/brick_lang.py
-Task 7. /homework_7/find_n_words_sentences.py
--- homework_7/data/download_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash/
-
-wget https://raw.githubusercontent.com/Serfentum/bf_course/master/15.re/references
-wget https://raw.githubusercontent.com/Serfentum/bf_course/master/15.re/2430AD
