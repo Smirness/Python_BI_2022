d661d3dbb94a184cc0c7dcf0c35563cd5032f993
smirness <smirnovviik@gmail.com>
2022-12-14 02:58:10 +0300

    Corrected according to the review

--- homework_2/fastq_filtrator.py
+++ homework_2/fastq_filtrator.py
@@ -1,5 +1,9 @@
+import os
+import sys
+
+
 # The main filter function
-def main(input_fastq, output_file_prefix, i=0, j=4,
+def main(input_fastq, output_file_prefix, i=0,
          gc_bounds=(0, 100), length_bounds=(0, 2 ** 32), quality_threshold=0, save_filtered=False):
     """
     Filter reeds by GC composition, length, quality.
@@ -8,11 +12,18 @@ def main(input_fastq, output_file_prefix, i=0, j=4,
             quality_threshold=0, save_filtered=False
     :return: None
     """
+    # check if file empty
+    if os.stat(input_fastq).st_size == 0:
+        print('File is empty')
+        sys.exit(0)
+
+    j = i + 4
     # numbers of lines in input_fastq
     num_lines_file_in = num_lines(input_fastq)
 
     # unification of parameters
-    min_gc_bounds, max_gc_bounds, min_length_bounds, max_length_bounds = fix_set(gc_bounds, length_bounds)
+    min_gc_bounds, max_gc_bounds = fix_set(gc_bounds)
+    min_length_bounds, max_length_bounds = fix_set(length_bounds)
 
     # lines extraction for filtering
     with open(input_fastq, "r") as file_in:
@@ -34,7 +45,7 @@ def main(input_fastq, output_file_prefix, i=0, j=4,
 
     # recursive call to read all lines in input_fastq
     if j <= (num_lines_file_in - 1):
-        main(input_fastq, output_file_prefix, i + 4, j + 4, gc_bounds, length_bounds, quality_threshold, save_filtered)
+        main(input_fastq, output_file_prefix, i + 4, gc_bounds, length_bounds, quality_threshold, save_filtered)
 
     return
 
@@ -46,8 +57,9 @@ def num_lines(input_fastq):
     :return: num_lines_file_in
     """
     with open(input_fastq) as file_in:
-        num_lines_file_in = len(file_in.readlines())
-    return num_lines_file_in
+        for i, line in enumerate(file_in):
+            pass
+    return i + 1
 
 
 def name_file(input_fastq):
@@ -56,30 +68,24 @@ def name_file(input_fastq):
     :param: input_fastq
     :return: name_output_file
     """
-    input_fastq = list(input_fastq)
-    name_output_file = "".join(input_fastq[:-6])
+    name_output_file = input_fastq.removesuffix(".fastq")
 
     return name_output_file
 
 
-def fix_set(gc_bounds, length_bounds):
+def fix_set(bounds):
     """
     Filter reeds by GC composition, length, quality
     :param: gc_bounds, length_bounds
     :return: min_gc_bounds, max_gc_bounds, min_length_bounds, max_length_bounds
     """
 
-    if isinstance(gc_bounds, int) is True:
-        min_gc_bounds, max_gc_bounds = 0, gc_bounds
+    if isinstance(bounds, int) or isinstance(bounds, float):
+        min_bound, max_bound = 0, bounds
     else:
-        min_gc_bounds, max_gc_bounds = gc_bounds
+        min_bound, max_bound = bounds
 
-    if isinstance(length_bounds, int) is True:
-        min_length_bounds, max_length_bounds = 0, length_bounds
-    else:
-        min_length_bounds, max_length_bounds = length_bounds
-
-    return min_gc_bounds, max_gc_bounds, min_length_bounds, max_length_bounds
+    return min_bound, max_bound
 
 
 def filtr_gc(line_1, min_gc_bounds, max_gc_bounds):
@@ -88,14 +94,10 @@ def filtr_gc(line_1, min_gc_bounds, max_gc_bounds):
     :param: line_1, min_gc_bounds, max_gc_bounds
     :return: status (True/False)
     """
-    status = False
 
     cg = (line_1.count("C") + line_1.count("G")) / (len(line_1) - 1) * 100
 
-    if min_gc_bounds <= cg <= max_gc_bounds:
-        status = True
-
-    return status
+    return min_gc_bounds <= cg <= max_gc_bounds
 
 
 def filtr_len(line_1, min_length_bounds, max_length_bounds):
@@ -104,12 +106,8 @@ def filtr_len(line_1, min_length_bounds, max_length_bounds):
     :param: line_1, min_length_bounds, max_length_bounds
     :return: status (True/False)
     """
-    status = False
 
-    if min_length_bounds <= (len(line_1) - 1) <= max_length_bounds:
-        status = True
-
-    return status
+    return min_length_bounds <= (len(line_1) - 1) <= max_length_bounds
 
 
 def filtr_qual(line_3, quality_threshold):
@@ -118,35 +116,15 @@ def filtr_qual(line_3, quality_threshold):
     :param: line_3, quality_threshold
     :return: status (True/False)
     """
-    acsii_table = {
-        "33": "0", "34": "1", "35": "2", "36": "3", "37": "4", "38": "5", "39": "6", "40": "7", "41": "8", "42": "9",
-        "43": "10", "44": "11", "45": "12", "46": "13", "47": "14", "48": "15", "49": "16", "50": "17", "51": "18",
-        "52": "19",
-        "53": "20", "54": "21", "55": "22", "56": "23", "57": "24", "58": "25", "59": "26", "60": "27", "61": "28",
-        "62": "29",
-        "63": "30", "64": "31", "65": "32", "66": "33", "67": "34", "68": "35", "69": "36", "70": "37", "71": "38",
-        "72": "39", "73": "40"
-    }
 
-    status = False
-    line_ascii = []
-    sum_q_score = 0
-    aver_q_score = 0
+    line3 = line_3.strip()
+    total_quality = 0
 
-    for sym in line_3:
-        ascii_code = ord(sym)
-        line_ascii.append(ascii_code)
-    line_ascii = line_ascii[:-1]
+    for sym in line3:
+        total_quality += ord(sym) - 33
+    average_quality = total_quality / len(line3)
 
-    for ascii_code in line_ascii:
-        q_score = acsii_table[str(ascii_code)]
-        sum_q_score += int(q_score)
-        aver_q_score = sum_q_score / len(line_ascii)
-
-    if aver_q_score >= quality_threshold:
-        status = True
-
-    return status
+    return average_quality >= quality_threshold
 
 
 def saving(status_gc, status_len, status_q, save_filtered,
